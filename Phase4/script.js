9eea03098d16fd26ca9c1b9020533763314a1500
Periyasamy A <146866752+PeriyasamyA@users.noreply.github.com>
2023-10-26 10:43:21 +0530

    Add files via upload

--- Phase4/script.js
+++ Phase4/script.js
@@ -1,33 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Simulated data, replace with your dataset
-    const data = {
-        daily: {
-            date: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
-            consumption: [10, 15, 12, 18, 14, 16, 20]
-        },
-        weekly: 110
+    // Sample data (replace with your dataset)
+    const dailyData = {
+        labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
+        datasets: [{
+            label: "Daily Consumption",
+            data: [100, 150, 120, 180, 140, 160, 200],
+            fill: false,
+            borderColor: "blue"
+        }]
     };
 
-    // Update daily and weekly consumption
-    document.getElementById("daily-consumption").innerText = `Total: ${data.daily.consumption[data.daily.consumption.length - 1]} gallons`;
-    document.getElementById("weekly-consumption").innerText = `Total: ${data.weekly} gallons`;
+    const monthlyData = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+        datasets: [{
+            label: "Monthly Consumption",
+            data: [3000, 3200, 3100, 3300, 3200, 3100, 3350],
+            fill: false,
+            borderColor: "green"
+        }]
+    };
 
-    // Create a line chart
-    const ctx = document.getElementById("consumption-chart").getContext("2d");
-    new Chart(ctx, {
-        type: "line",
-        data: {
-            labels: data.daily.date,
-            datasets: [{
-                label: "Daily Consumption",
-                data: data.daily.consumption,
-                fill: false,
-                borderColor: "blue"
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
+    createLineChart("daily-chart", dailyData);
+    createLineChart("monthly-chart", monthlyData);
+
+    function createLineChart(canvasId, data) {
+        const ctx = document.getElementById(canvasId).getContext("2d");
+        new Chart(ctx, {
+            type: "line",
+            data: data,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
 });
